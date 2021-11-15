074f1cdb918937a6a1fbc8d46392507526dddf64
Yetife <tifetreats21@gmail.com>
2021-11-15 11:15:25 +0100

    Logistics system

--- src/main/java/africa/semicolon/cheetah/utils/ModelMapper.java
+++ src/main/java/africa/semicolon/cheetah/utils/ModelMapper.java
@@ -1,16 +1,18 @@
 package africa.semicolon.cheetah.utils;
 import africa.semicolon.cheetah.data.models.Package;
+import africa.semicolon.cheetah.data.models.Sender;
 import africa.semicolon.cheetah.dtos.requests.AddPackageRequest;
+import africa.semicolon.cheetah.dtos.requests.RegisterSenderRequest;
 import africa.semicolon.cheetah.dtos.responses.AddPackageResponse;
+import africa.semicolon.cheetah.dtos.responses.RegisterSenderResponse;
 
 public class ModelMapper {
     public static Package map(AddPackageRequest addPackageRequest)
     {
         Package apackage = new Package();
         apackage.setName(addPackageRequest.getPackageDescription());
-        apackage.setSenderPhone(addPackageRequest.getSenderPhone());
+        apackage.setSenderEmail(addPackageRequest.getSenderEmail());
         apackage.setDeliveryAdresss(addPackageRequest.getDeliveryAdresss());
-        apackage.setSenderName(addPackageRequest.getSenderName());
         apackage.setReceiverPhone(addPackageRequest.getReceiverPhone());
         apackage.setReceiverName(addPackageRequest.getReceiverName());
         apackage.setNetWeight(addPackageRequest.getPackageWeight());
@@ -27,6 +29,23 @@ public class ModelMapper {
         response.setTrackingNumber(savedPackage.getId());
         response.setReceiverPhone(savedPackage.getReceiverPhone());
 
+        return response;
+    }
+
+    public static Sender map(RegisterSenderRequest registerSenderRequest)
+    {
+        Sender sender = new Sender();
+        sender.setSenderName(registerSenderRequest.getSenderName());
+        sender.setPhoneNumber(registerSenderRequest.getPhoneNumber());
+        sender.setEmailAddress(registerSenderRequest.getSenderEmail());
+
+        return sender;
+    }
+
+    public static RegisterSenderResponse map(Sender sender)
+    {
+        RegisterSenderResponse response = new RegisterSenderResponse();
+        response.setSenderEmail(sender.getEmailAddress());
         return response;
     }
 }
