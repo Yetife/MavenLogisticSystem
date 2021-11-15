cf7a0fab5e4c9db4ac565489b6d3d7dd3a0a295f
Yetife <tifetreats21@gmail.com>
2021-11-15 23:24:41 +0100

    Logistics system file

---
+++ src/main/java/africa/semicolon/cheetah/exceptions/CheetahAppException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.cheetah.exceptions;
+
+public class CheetahAppException extends RuntimeException{
+    public CheetahAppException(String message) {
+        super(message);
+    }
+}
